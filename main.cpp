3ceb19d2fb1003b54a090bb4ff5c059af177c19d
9z9z9z9z <rolyan293@gmail.com>
2024-10-05 17:06:26 +0300

    Add step-by-step file reading and calculating hash sum

--- main.cpp
+++ main.cpp
@@ -1,132 +1,152 @@
 #include <iostream>
 #include <fstream>
-#include <cstdint>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <algorithm>
 
 #include "colors.h"
+#include <bitset>
 
-static std::string PATH = "path";
-static std::string HASH = "hash";
 
-struct JsonFile {
-    std::string filePath;
-    uint32_t hashSum;
-    
-    JsonFile(const std::string &rFilePath, uint32_t rHashSum) {
-        filePath = rFilePath;
-        hashSum = rHashSum;
-    }
-};
+const static std::string PATH = "path";
+const static std::string HASH = "hash";
 
-uint32_t crc32(const void *data, uint32_t lastVal, size_t size) {
-    const uint32_t startingPoly = 0xEDB88320;
-    uint32_t hashSum = ~lastVal;
-    const unsigned char *buff = reinterpret_cast<const unsigned char*>(data);
+namespace CRC {
+    uint32_t crc32(const void *data, uint32_t lastVal, size_t size) {
+        const uint32_t startingPoly = 0xEDB88320;
+        uint32_t hashSum = ~lastVal;
+        const unsigned char *buff = reinterpret_cast<const unsigned char*>(data);
 
-    while (size--) {
-        hashSum = hashSum ^ *buff;
-        ++buff;
-        for (uint8_t bit = 0; bit < 8; ++bit) {
-            if (hashSum & 1) {
-                hashSum = (hashSum >> 1) ^ startingPoly;
-            } else {
-                hashSum = (hashSum >> 1);
+        while (size--) {
+            hashSum = hashSum ^ uint32_t(*buff);
+            ++buff;
+            for (uint8_t bit = 0; bit < 8; ++bit) {
+                if ((hashSum & uint32_t(1)) != 0) {
+                    hashSum = (hashSum >> 1) ^ startingPoly;
+                } else {
+                    hashSum = (hashSum >> 1);
+                }
             }
         }
-    }
 
-    return ~hashSum;
+        return ~hashSum;
+    }
 }
 
-int checkCurrentFile(const std::string &filePath, const uint32_t hashSum) {
-    int retCode = 1;
-    size_t currentBit = 0;
-    std::ifstream file(filePath, std::ifstream::binary);
-    if (!file.is_open()) {
-        std::cerr << KRED << "Cannot open " << filePath << " file\n" << RST;
-        retCode = -1;
-    } else {
-        uint32_t hash = 0x00000000;
-        file.seekg(0, file.end);
-        size_t fileLength = file.tellg();
-        file.seekg(0, file.beg);
-        char *tmp = new char[fileLength];
-        file.read(tmp, fileLength);
-        file.close();
-
-        hash = crc32(tmp, hash, fileLength);
+namespace JsonFuncs {
+    struct JsonFile {
+        std::string filePath;
+        uint32_t hashSum;
         
-        if (hash != hashSum) {
-            std::cerr   << "\nFile " << filePath << " was changed\n";
-            std::cerr   << "Calculated hash is " << std::hex << hash
-                        << " expected " << std::hex << hashSum << '\n';
-            retCode = 0;
+        JsonFile(const std::string &rFilePath, uint32_t rHashSum) {
+            filePath = rFilePath;
+            hashSum = rHashSum;
+        }
+    };
+
+    int checkCurrentFile(const std::string &filePath, const uint32_t hashSum) {
+        int retCode = 0;
+        size_t currentBit = 0;
+        std::ifstream file(filePath, std::ifstream::binary);
+        if (!file.is_open()) {
+            std::cerr << KRED << "Cannot open " << filePath << " file\n" << RST;
+            retCode = -1;
         } else {
-            std::cout   << "\nFile " << filePath << " is correct\n";
-            std::cout   << "Calculated hash is " << std::hex << hash
-                        << " expected " << std::hex << hashSum << '\n';
+            uint32_t hash = 0x00000000;
+            file.seekg(0, file.end);
+            size_t fileLength = file.tellg();
+            file.seekg(0, file.beg);
+            size_t buffSize = 32;            
+            size_t readedBits = 0;
+            char *buff = new char[buffSize];
+            while (readedBits + buffSize < fileLength) {
+                file.read(buff, buffSize);
+                readedBits += buffSize;
+                hash = CRC::crc32(buff, hash, buffSize);
+            }
+            delete buff;
+            if (readedBits < fileLength) {
+                buffSize = fileLength - readedBits;
+                buff = new char[buffSize];
+                file.read(buff, buffSize);
+                hash = CRC::crc32(buff, hash, buffSize);
+            }
+            delete buff;
+
+            if (hash != hashSum) {
+                std::cerr   << "\nFile " << filePath << " was changed\n";
+                std::cerr   << "Calculated hash is " << std::hex << hash
+                            << " expected " << std::hex << hashSum << '\n';
+                retCode = 1;
+            } else {
+                std::cout   << "\nFile " << filePath << " is correct\n";
+                std::cout   << "Calculated hash is " << std::hex << hash
+                            << " expected " << std::hex << hashSum << '\n';
+            }
         }
+        return retCode;
     }
-    return retCode;
-}
 
-std::string extractValue(const std::string &line, const std::string &key) {
-    size_t pos = line.find(key);
-    if (line.data() == nullptr || &key == nullptr) {
-        std::cerr << KRED << "Cannot extract " << key << " from " << line << '\n' << RST;
-        return "";
-    }
-    if (pos == std::string::npos) {
-        std::cerr << KRED << "Cannot find " << key << " in " << line << '\n' << RST;
-        return "";
-    }
-    pos += key.length();
-    ++pos;
-    pos = line.find(":", pos);
-    if (pos == std::string::npos) {
-        std::cerr << KRED << R"(Cannot find ":" in )" << line << '\n' << RST;
-        return "";
-    }
-    ++pos;
-
-    while (pos < line.size() && (line[pos] == ' ' || line[pos] == '"')) {
+    std::string extractValue(const std::string &line, const std::string &key) {
+        size_t pos = line.find(key);
+        if (line.data() == nullptr) {
+            std::cerr << KRED << "Cannot extract " << key << " from " << line << '\n' << RST;
+            return "";
+        }
+        if (pos == std::string::npos) {
+            std::cerr << KRED << "Cannot find " << key << " in " << line << '\n' << RST;
+            return "";
+        }
+        pos += key.length();
         ++pos;
+        pos = line.find(":", pos);
+        if (pos == std::string::npos) {
+            std::cerr << KRED << R"(Cannot find ":" in )" << line << '\n' << RST;
+            return "";
+        }
+        ++pos;
+
+        while (pos < line.size() && (line[pos] == ' ' || line[pos] == '"')) {
+            ++pos;
+        }
+
+        size_t endPos = line.find_first_of(",", pos);
+        if (endPos == std::string::npos) {
+            endPos = line.size();
+        }
+
+        std::string value = line.substr(pos, endPos - pos);
+        value.erase(remove(value.begin(), value.end(), '"'), value.end());
+        return value;
     }
 
-    size_t endPos = line.find_first_of(",", pos);
-    if (endPos == std::string::npos) {
-        endPos = line.size();
+    std::vector<JsonFile> parseJsonFile(std::ifstream &file) {
+        std::vector<JsonFile> jsonFiles;
+        std::string line;
+        std::stringstream ss;
+        std::string path;
+        uint32_t hash;
+        while (std::getline(file, line)) {
+            if (line.find("},") != std::string::npos || line.find("]") != std::string::npos) {
+                jsonFiles.push_back(JsonFile(path, hash));
+                path = "";
+                hash = 0;
+                ss << std::flush;
+            }
+            if (line.find(PATH) != std::string::npos) {
+                path = extractValue(line, PATH);
+            }
+            if (line.find(HASH) != std::string::npos) {
+                std::string val = extractValue(line, HASH);
+                if (!val.empty()) {
+                    hash = static_cast<uint32_t>(std::stoul(val, nullptr, 16));
+                }            
+            }
+        }
+        return jsonFiles;
     }
 
-    std::string value = line.substr(pos, endPos - pos);
-    value.erase(remove(value.begin(), value.end(), '"'), value.end());
-    return value;
-}
-
-std::vector<JsonFile> parseJsonFile(std::ifstream &file) {
-    std::vector<JsonFile> jsonFiles;
-    std::string line;
-    std::stringstream ss;
-    std::string path;
-    uint32_t hash;
-    while (std::getline(file, line)) {
-        if (line.find("},") != std::string::npos || line.find("]") != std::string::npos) {
-            jsonFiles.push_back(JsonFile(path, hash));
-            path = "";
-            hash = 0;
-            ss << std::flush;
-        }
-        if (line.find(PATH) != std::string::npos) {
-            path = extractValue(line, PATH);
-        }
-        if (line.find(HASH) != std::string::npos) {
-            hash = static_cast<uint32_t>(std::stoul(extractValue(line, HASH), nullptr, 16));
-        }
-    }
-    return jsonFiles;
 }
 
 bool hashSumChecking(const std::string &jsonFilePath) {
@@ -135,10 +155,12 @@ bool hashSumChecking(const std::string &jsonFilePath) {
         std::cerr << KRED << "Cannot open " << jsonFilePath << '\n' << RST;
         return false;
     } else {
-        std::vector<JsonFile> files = parseJsonFile(file);
+        std::vector<JsonFuncs::JsonFile> files = JsonFuncs::parseJsonFile(file);
         size_t count = 0;
-        for (std::vector<JsonFile>::iterator it = files.begin(); it != files.end(); ++it) {
-            count += checkCurrentFile(it->filePath, it->hashSum);
+        for (std::vector<JsonFuncs::JsonFile>::iterator it = files.begin(); it != files.end(); ++it) {
+            if (JsonFuncs::checkCurrentFile(it->filePath, it->hashSum) == 0) {
+                ++count;
+            }
         }
         file.close();
         return count == files.size();
@@ -151,12 +173,14 @@ int test() {
     std::cout << FYEL("==============================================================\n");
     std::cout << "Testing crc32 algorythm (string 123456789):\n";
     {
-        std::string testStr1 = "12345";
-        std::string testStr2 = "6789";
-        std::string testStr = testStr1 + testStr2;
-        uint32_t testTmpHash = crc32(testStr1.data(), 0x00000000, testStr1.length());
-        uint32_t testHalfHash = crc32(testStr2.data(), testTmpHash, testStr2.length());
-        uint32_t testSingleHash = crc32(testStr.data(), 0x00000000, testStr.length());
+        std::string testStr1 = "123";
+        std::string testStr2 = "4567";
+        std::string testStr3 = "89";
+        std::string testStr = testStr1 + testStr2 + testStr3;
+        uint32_t testTmpHash = CRC::crc32(testStr1.data(), uint32_t(0x00000000), testStr1.length());
+        uint32_t testHalfHash = CRC::crc32(testStr2.data(), testTmpHash, testStr2.length());
+        testHalfHash = CRC::crc32(testStr3.data(), testHalfHash, testStr3.length());
+        uint32_t testSingleHash = CRC::crc32(testStr.data(), uint32_t(0x00000000), testStr.length());
         std::cout << "Half string-> " << std::hex << testHalfHash << '\n';
         std::cout << "Single string-> " << std::hex << testSingleHash << '\n';
         if (testSingleHash == testHalfHash) {
@@ -173,22 +197,16 @@ int test() {
         if (!file.is_open()) {
             std::cerr << "Missing testing file...\n";
         } else {
+            file.close();
             uint32_t test_Hash = 0xf4c9c4c8;
-            uint32_t test_calculatedHash;
-            file.seekg(0, file.end);
-            size_t test_FileLength = file.tellg();
-            file.seekg(0, file.beg);
-            char *test_FileData = new char[test_FileLength];
-            file.read(test_FileData, test_FileLength);
-            test_calculatedHash = crc32(test_FileData, 0x00000000, test_FileLength);
-            std::cout << "Calculated hash: " << std::hex << test_calculatedHash << '\n';
-            if (test_Hash == test_calculatedHash) {
+            int retCode = JsonFuncs::checkCurrentFile("config.json", test_Hash);    
+
+            if (retCode == 0) {
                 std::cout << FGRN("Test passed") << '\n';
             } else {
                 std::cerr << FRED ("Test failed") << '\n';
                 test_flag = 1;
             }
-            file.close();
         }
     }
     std::cout << FYEL("==============================================================\n");
@@ -208,13 +226,13 @@ int test() {
 int main(int argc, char **argv) {
     if (std::string(argv[1]) == "-test") {
         return test();
-    } else  {
-        if (hashSumChecking(argv[1])) {
-            std::cout << KGRN << "\nAll files are correct\n" << RST << std::endl;
-        } else {
-            std::cout << KRED << "\nSome files were changed\n" << RST << std::endl;
-        }
-        return 0;
-    }    
-    
+    }
+    //else  {
+    //     if (hashSumChecking(argv[1])) {
+    //         std::cout << KGRN << "\nAll files are correct\n" << RST << std::endl;
+    //     } else {
+    //         std::cout << KRED << "\nSome files were changed\n" << RST << std::endl;
+    //     }
+    //     return 0;
+    // }    
 }
