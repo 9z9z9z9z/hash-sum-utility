d124e53e9686b6085f409cce5c7927cdef2e654b
9z9z9z9z <rolyan293@gmail.com>
2024-10-05 19:58:56 +0300

    Add exception handling

---
+++ Exceptions.h
@@ -0,0 +1,45 @@
+#ifndef _EXCEPTIONS_
+#define _EXCEPTIONS_
+
+#include <exception>
+#include <string>
+
+namespace Exceptions {
+
+class FILE_OPEN_ERR : public std::exception {
+    public:
+        FILE_OPEN_ERR(const std::string &messange) {
+            _messange = messange;
+        }
+        const char* what() const noexcept override {
+            return _messange.c_str();
+        }
+    private:
+        std::string _messange;
+};
+
+class JSON_VAL_EXTRUCT_ERR : public std::exception {
+    public:
+        JSON_VAL_EXTRUCT_ERR(const std::string &message) {
+            _message = message;
+        }
+        const char* what () const noexcept override {
+            return _message.c_str();
+        }
+    private:
+        std::string _message;
+};
+
+class JSON_IN_LINE_ERR : public std::exception {
+    public:
+        JSON_IN_LINE_ERR(const std::string &message) {
+            _message = message;
+        }
+        const char* what () const noexcept override {
+            return _message.c_str();
+        }
+    private:
+        std::string _message;
+};
+}
+#endif
